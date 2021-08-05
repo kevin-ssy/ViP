0c5ea5579e85249313cdc104001c6c70734ca636
kevin-ssy <kevinsun@robots.ox.ac.uk>
2021-08-05 02:35:27 +0100

    add dist support

--- utils/distributed_utils.py
+++ utils/distributed_utils.py
@@ -1,7 +1,8 @@
 import os
 import torch
+import random
 import multiprocessing
-
+import numpy as np
 import torch.distributed as t_dist
 
 
@@ -35,3 +36,36 @@ def dist_init(port=2333):
     t_dist.init_process_group(backend='nccl')
     
     return rank, int(world_size), gpu_id
+
+
+def init_device(args):
+    # Random seed setting
+    if args.seed is not None:
+        random.seed(args.seed)
+        np.random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        torch.cuda.manual_seed_all(args.seed)
+        torch.backends.cudnn.deterministic = True
+
+    if 'WORLD_SIZE' in os.environ:
+        args.distributed = int(os.environ['WORLD_SIZE']) > 1
+        if args.distributed and args.num_gpu > 1:
+            print('Using more than one GPU per process in distributed mode is not allowed.Setting num_gpu to 1.')
+            args.num_gpu = 1
+
+    args.device = int(os.environ['LOCAL_RANK'])
+
+    print(f"device: {args.device}")
+    torch.cuda.set_device(args.device)
+    t_dist.init_process_group(backend='nccl', init_method='env://')
+
+    rank = t_dist.get_rank()
+    size = t_dist.get_world_size()
+    device = torch.device(f'cuda:{args.device}')
+    print(f"args.num_gpu: {args.num_gpu}, rank: {rank}, world_size: {size}")
+
+    print(f'=> Device: running distributed training with world size:{size}')
+    # PyTorch version record
+    print(f'=> PyTorch Version: {torch.__version__}\n')
+    t_dist.barrier()
+    return rank, size, device
